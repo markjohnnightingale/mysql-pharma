db0e702d899968fbf91e858398f48358bd65336a
Mark Nightingale <mark.john.nightingale@gmail.com>
2014-01-23 17:58:23 +0100

    connection

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
 /* rajouter un article au panier*/
 $dbuser = 'root';
-$dbpass = '';
+$dbpass = '77ymfR!';
 
 $conn = new PDO('mysql:host=localhost;dbname=pharma;charset=utf8', $dbuser, $dbpass);
 ?>
