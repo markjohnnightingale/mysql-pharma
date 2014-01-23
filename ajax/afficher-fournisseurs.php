853bc7b9099dfc0398f3d7f1e0d4fc3160c39e84
Marina-B <marinabonhomme@gmail.com>
2014-01-23 11:43:47 +0100

    Edition bis

--- ajax/afficher-fournisseurs.php
+++ ajax/afficher-fournisseurs.php
@@ -3,17 +3,15 @@ require "../connect.php";
 
 $sql = 'SELECT `id_fournisseur`, `nom_fournisseur`, `personne_contact`,`adresse`,`ville`,`code_postal`,`tel`,`email` FROM fournisseur'; // SQL Query
 foreach ($conn->query($sql) as $row) { // Loop through each row and for each row display table layout
-  print '<td>'.$row['nom_fournisseur'].' <span class="secondary radius label">'.$row['id_fournisseur'].'</span></td>'; // Name of supplier and ID label
+  print '<tr>';
+  print '<td><a href="index.php?page=fournisseur&id='.$row['id_fournisseur'].'">'.$row['nom_fournisseur'].'</a> <span class="secondary radius label">'.$row['id_fournisseur'].'</span></td>'; // Name of supplier and ID label
   print '<td>'.$row['personne_contact'].'</td>';
   print '<td>'.$row['adresse'].'</td>';
   print '<td>'.$row['ville'].'</td>';
   print '<td>'.$row['code_postal'].'</td>';
   print '<td>'.$row['tel'].'</td>';
   print '<td>'.$row['email'].'</td>';
-  $sqlFournisseur = 'SELECT `nom_fournisseur` FROM fournisseur WHERE `id_fournisseur` LIKE \''.$row['fournisseur'].'\'';
-  foreach($conn->query($sqlFournisseur) as $rowFournisseur){
-  	  print '<td><a href="index.php?page=fournisseur&id='.$row['fournisseur'].'">'.$rowFournisseur['nom_fournisseur'].'</a></td>'; //Fournisseur ID and encoded into URLso that onclick takes you to page with more details
-  };
+  print '</tr>';
 };
 ?>
 
