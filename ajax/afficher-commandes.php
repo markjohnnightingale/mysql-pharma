187f0ee477b0667d778215167af4ffa76ca4a249
Mark Nightingale <mark.john.nightingale@gmail.com>
2014-01-30 10:18:52 +0100

    Medicaments table

--- ajax/afficher-commandes.php
+++ ajax/afficher-commandes.php
@@ -6,7 +6,7 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
 $sql = 'SELECT `id_commande`, `date`, `client`,`mode_reglement` FROM commande'; // SQL Query
 foreach ($conn->query($sql) as $row) { // Loop through each row and for each row display table layout
   print '<tr>';
-  print '<td><a href="index.php?page=visualiser-commande&id='.$row['id_commande'].'"> Visualiser Cmd Id: '.$row['id_commande'].'</a></td>';
+  print '<td><a href="index.php?page=visualiser-commande&id='.$row['id_commande'].'">Commande N˚ '.$row['id_commande'].'</a></td>';
   print '<td>'.$row['date'].'</td>';
   $sqlClient = 'SELECT `no_client`, `civilite`, `prenom`, `nom`, `adresse`,`ville`,`code_postal` FROM clients WHERE `no_client` LIKE \''.$row['client'].'\'';
   foreach($conn->query($sqlClient) as $rowClient){
@@ -18,20 +18,23 @@ foreach ($conn->query($sql) as $row) { // Loop through each row and for each row
 		</td>'; 
   };
   print "<td>";
-  print "<ul>";
+  print "<table>";
   $prixCommande = 0;
   $sqlMeds = 'SELECT `id_med`, `qte` FROM commande_medicaments WHERE `id_commande` LIKE \''.$row['id_commande'].'\'';
   foreach($conn->query($sqlMeds) as $rowMeds){
-  	  print "<li>".$rowMeds['qte']." x ";
+  	  print "<tr><td>".$rowMeds['qte']."</td><td> x </td><td>";
 	  $sqlNomMed = 'SELECT `nom_med`, `prix` FROM medicament WHERE `id_med` LIKE \''.$rowMeds['id_med'].'\'';
 	  foreach($conn->query($sqlNomMed) as $med) {
-		  print $med['nom_med']."</li>";
+		  print '<a href="index.php?page=modifier-medicament&id='.$rowMeds['id_med'].'">'.$med['nom_med']."</a></td>";
 		  $prixCommande += $rowMeds['qte'] * $med['prix']; 
 	  }
   };
-  print '</ul></td>'; // ordonnance
+  print '</table></td>'; // ordonnance
   print '<td>'.number_format($prixCommande,2).' € </td>'; // prix total
-  print '<td>'.$row['mode_reglement'].'</td>';
-  print '</tr>';
+  
+  if ($row['mode_reglement'] == "cb") {  print '<td>Carte Bancaire</td>';}
+  if ($row['mode_reglement'] == "cheque") {  print '<td>Chèque</td>';}
+  if ($row['mode_reglement'] == "especes") {  print '<td>Espèces</td>';}
+    print '</tr>';
 };
 ?>
